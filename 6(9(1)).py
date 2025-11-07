192ba70f7b9d0c5c886d4904bdb71651b074b124
EreminaAl <ereminaa230@gmail.com>
2025-11-07 18:42:09 +0300

    Update 6(9(1)).py

--- 6(9(1)).py
+++ 6(9(1)).py
@@ -1,12 +1,6 @@
-s= input("Введите вещественные числа через пробел: ")
-numbers = list(map(float, s.split()))
-min_x_value = x(numbers[0])
-min_element = numbers[0]
-for number in numbers:
-    x_value= x(number)
-    if x_value < min_x_value:
-        min_x_value = x_value
-        min_element = number 
-print("Минимальный по модулю элемент:", min_element)
-reversed_numbers = numbers[::-1]
-print("Массив в обратном порядке:", reversed_numbers)
+N=int(input('Введите количество элементов массива: '))
+num=[]
+for i in range(N):
+    num.append(float(input('Введите элемент: ')))
+min_elements=min(num, key=abs)
+print(min_elements, num[::-1])
